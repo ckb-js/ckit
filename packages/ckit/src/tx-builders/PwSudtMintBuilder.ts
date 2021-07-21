ca0aeefed7caa9ca2353dfb27d510ab9789eb56b
homura <homura.dev@gmail.com>
2021-07-21 19:35:11 +0800

    feat: supported batch mint

--- packages/ckit/src/tx-builders/PwSudtMintBuilder.ts
+++ packages/ckit/src/tx-builders/PwSudtMintBuilder.ts
@@ -1,13 +1,26 @@
 import { Address, HexNumber, Transaction } from '@ckb-lumos/base';
-import { CkbTypeScript, TransactionBuilder } from '../interfaces';
+import { TransactionBuilder } from '../interfaces';
 import { MercuryProvider } from '../providers/MercuryProvider';
 import { unimplemented } from '../utils';
 
+type CapacityPolicy =
+  // mint only when recipient has ACP cell
+  | 'findAcp'
+  // mint and create an ACP cell for recipient
+  | 'createAcp'
+  // find the recipient's ACP cell, and if not find it, create a new ACP for the recipient
+  | 'findOrCreateAcp';
+
+type RecipientOptions = {
+  recipient: Address;
+  amount: HexNumber;
+  additionalCapacity: HexNumber;
+  capacityPolicy: CapacityPolicy;
+};
+
 export interface MintOptions {
   issuer: Address;
-  recipient: Address;
-  sudt: CkbTypeScript;
-  amount: HexNumber;
+  recipients: RecipientOptions[];
 }
 
 export class PwSudtMintBuilder implements TransactionBuilder {
