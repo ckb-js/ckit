ec1210e516aa8442cd620ab122357487082ba66c
Yonghui Lin <homura.dev@gmail.com>
2021-07-27 19:47:26 +0800

    chore: avoid regenerator-runtime error when running with ts-node (#14)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['packages'],
-  setupFiles: ['regenerator-runtime/runtime'],
   testMatch: ['<rootDir>/**/*.spec.ts'],
   globals: {
     'ts-jest': {
