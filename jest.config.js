3e09bd1fcd845030ee59f3da975da2e0bc8b204b
homura <homura.dev@gmail.com>
2021-07-27 14:43:21 +0800

    fix: test failed

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['packages'],
+  setupFiles: ['regenerator-runtime/runtime'],
   testMatch: ['<rootDir>/**/*.spec.ts'],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.build.json'
+      tsconfig: 'tsconfig.build.json',
+      babelConfig: true,
     },
   },
 };
