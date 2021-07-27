d2310efa0ff2ba265fe64ff86ffb5b0150207671
jacobdenver007 <jacobdenver@163.com>
2021-07-27 17:01:51 +0800

    fix: add mercury client

--- packages/mercury-client/src/index.ts
+++ packages/mercury-client/src/index.ts
@@ -1,3 +1,4 @@
+import { Tip } from '@ckb-lumos/base';
 import { Client, HTTPTransport, RequestManager } from '@open-rpc/client-js';
 
 export class MercuryClient {
@@ -6,5 +7,15 @@ export class MercuryClient {
   constructor(uri = 'http://127.0.0.1:5000/api') {
     const transport = new HTTPTransport(uri, { headers: { 'content-type': 'application/json' } });
     this.client = new Client(new RequestManager([transport]));
+  }
+
+  public async getTip(): Promise<Tip> {
+    const res = await this.client.request({ method: 'get_tip' });
+    return res as Tip;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+  public async getCells(params: any): Promise<any> {
+    return await this.client.request({ method: 'get_cells', params: params });
   }
 }
