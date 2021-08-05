951782bb003736c721831350f749bc9847dba26c
homura <homura.dev@gmail.com>
2021-08-05 19:41:50 +0800

    feat: a helper for pw model

---
+++ packages/ckit/src/helpers/pw.ts
@@ -0,0 +1,84 @@
+import {
+  Cell as LumosCell,
+  CellDep as LumosCellDep,
+  Script as LumosScript,
+  OutPoint as LumosOutPoint,
+} from '@ckb-lumos/base';
+import {
+  Amount,
+  Cell as PwCell,
+  CellDep as PwCellDep,
+  DepType as PwDepType,
+  HashType,
+  OutPoint as PwOutPoint,
+  Script as PwScript,
+} from '@lay2/pw-core';
+
+function fromPwCellDep(x: PwCellDep): LumosCellDep {
+  return {
+    dep_type: x.depType,
+    out_point: {
+      tx_hash: x.outPoint.txHash,
+      index: x.outPoint.index,
+    },
+  };
+}
+
+function toPwCellDep(x: LumosCellDep): PwCellDep {
+  return new PwCellDep(
+    x.dep_type === 'dep_group' ? PwDepType.depGroup : PwDepType.code,
+    new PwOutPoint(x.out_point.tx_hash, x.out_point.index),
+  );
+}
+
+function fromPwScript(x: PwScript): LumosScript {
+  return {
+    code_hash: x.codeHash,
+    args: x.args,
+    hash_type: x.hashType,
+  };
+}
+
+function toPwScript(x: LumosScript): PwScript {
+  return new PwScript(x.code_hash, x.args, x.hash_type === 'type' ? HashType.type : HashType.data);
+}
+
+function fromPwCell(x: PwCell): LumosCell {
+  return {
+    cell_output: {
+      capacity: x.capacity.toHexString(),
+      type: x.type ? fromPwScript(x.type) : undefined,
+      lock: fromPwScript(x.lock),
+    },
+    out_point: x.outPoint ? { tx_hash: x.outPoint.txHash, index: x.outPoint.index } : undefined,
+    data: x.getData(),
+  };
+}
+
+function toPwCell(x: LumosCell): PwCell {
+  return new PwCell(
+    new Amount(x.cell_output.capacity, 0),
+    toPwScript(x.cell_output.lock),
+    x.cell_output.type ? toPwScript(x.cell_output.type) : undefined,
+    x.out_point ? new PwOutPoint(x.out_point.tx_hash, x.out_point.index) : undefined,
+    x.data,
+  );
+}
+
+function toPwOutPoint(x: LumosOutPoint): PwOutPoint {
+  return new PwOutPoint(x.tx_hash, x.index);
+}
+
+/**
+ * from: PW => Lumos
+ * to: Lumos => PW
+ */
+export const Pw = {
+  fromPwCellDep,
+  toPwCellDep,
+  fromPwCell,
+  toPwCell,
+  toPwScript,
+  fromPwScript,
+  toPwOutPoint,
+};
