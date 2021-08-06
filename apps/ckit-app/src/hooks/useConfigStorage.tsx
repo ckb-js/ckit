fb4e6b48d9d9efe69d72b9aacf65f8bb9085253c
homura <homura.dev@gmail.com>
2021-08-06 14:09:54 +0800

    feat: predefined config for Aggron

--- apps/ckit-app/src/hooks/useConfigStorage.tsx
+++ apps/ckit-app/src/hooks/useConfigStorage.tsx
@@ -1,7 +1,5 @@
-import { ScriptConfig } from '@ckb-lumos/config-manager';
 import { useLocalStorage } from '@rehooks/local-storage';
-import { CkitInitOptions } from 'ckit';
-import { randomHexString } from 'ckit/dist/utils';
+import { CkitInitOptions, predefined } from 'ckit';
 import { useMemo } from 'react';
 
 export interface LocalConfig {
@@ -15,24 +13,8 @@ export interface LocalConfig {
 // TODO remove random when predefined config is ready
 export function useConfigStorage(): [LocalConfig, (newValue: LocalConfig) => void, () => void] {
   const initialConfig = useMemo<LocalConfig>(() => {
-    const randomScriptConfig = (): ScriptConfig => ({
-      HASH_TYPE: 'type',
-      DEP_TYPE: 'code',
-      CODE_HASH: randomHexString(64),
-      TX_HASH: randomHexString(64),
-      INDEX: '0x0',
-    });
-
-    const ckitConfig: CkitInitOptions = {
-      PREFIX: 'ckt',
-      SCRIPTS: {
-        ANYONE_CAN_PAY: randomScriptConfig(),
-        PW_NON_ANYONE_CAN_PAY: randomScriptConfig(),
-        PW_ANYONE_CAN_PAY: randomScriptConfig(),
-        SECP256K1_BLAKE160: randomScriptConfig(),
-        SUDT: randomScriptConfig(),
-      },
-    };
+    // TODO check network first
+    const ckitConfig: CkitInitOptions = predefined.Aggron;
     const mecuryRPC = 'http://127.0.0.1:8116';
     const ckbRPC = 'http://127.0.0.1:8114';
     const nervosExploreTxUrlPrefix = 'https://explorer.nervos.org/aggron/transaction/';
--- packages/ckit/src/predefined/aggron.ts
+++ packages/ckit/src/predefined/aggron.ts
@@ -17,7 +17,17 @@ export const SCRIPTS: CkitConfig['SCRIPTS'] = {
   SECP256K1_BLAKE160: predefined.AGGRON4.SCRIPTS.SECP256K1_BLAKE160!,
   ANYONE_CAN_PAY: predefined.AGGRON4.SCRIPTS.ANYONE_CAN_PAY!,
   SUDT: predefined.AGGRON4.SCRIPTS.SUDT!,
+
+  // TODO replace me when deployed
   PW_ANYONE_CAN_PAY: toScriptConfig(CHAIN_SPECS.Aggron.pwLock),
   // TODO replace me when deployed
   PW_NON_ANYONE_CAN_PAY: toScriptConfig(CHAIN_SPECS.Aggron.pwLock),
+  // TODO replace me when deployed
+  UNIPASS: {
+    INDEX: '0x0',
+    TX_HASH: '0x03dd2a5594ed2d79196b396c83534e050ba0ad07fa5c1cd61a7094f9fb60a592',
+    DEP_TYPE: 'code',
+    CODE_HASH: '0x124a60cd799e1fbca664196de46b3f7f0ecb7138133dcaea4893c51df5b02be6',
+    HASH_TYPE: 'type',
+  },
 };
