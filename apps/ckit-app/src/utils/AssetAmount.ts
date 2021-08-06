8cd1af97209a2f1e9db38b7602950662b210d2d9
mkxbl <vyclzn@gmail.com>
2021-08-06 19:13:39 +0800

    fix asset amount

---
+++ apps/ckit-app/src/utils/AssetAmount.ts
@@ -0,0 +1,31 @@
+import { BigNumber } from 'bignumber.js';
+
+export class AssetAmount {
+  rawAmount: BigNumber;
+  decimals: number;
+
+  constructor(rawAmount: string | BigNumber, decimals: number) {
+    this.rawAmount = new BigNumber(rawAmount);
+    this.decimals = decimals;
+  }
+
+  static fromRaw(rawAmount: string, decimals: number): AssetAmount {
+    return new AssetAmount(rawAmount, decimals);
+  }
+
+  static fromHumanize(humanizeAmount: string, decimals: number): AssetAmount {
+    const rawAmount = new BigNumber(humanizeAmount).times(10 ** decimals);
+    return new AssetAmount(rawAmount, decimals);
+  }
+
+  toRawString(): string {
+    return this.rawAmount.toString();
+  }
+
+  toHumanizeString(): string {
+    const humanizeAmount = this.rawAmount.times(10 ** -this.decimals);
+    const originDecimalPlaces = humanizeAmount.decimalPlaces();
+    const rounded = humanizeAmount.decimalPlaces(originDecimalPlaces, BigNumber.ROUND_FLOOR);
+    return rounded.toFormat().toString();
+  }
+}
