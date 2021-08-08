ad0039dc95f0f717621b66e3df59664d7f011e3a
homura <homura.dev@gmail.com>
2021-08-08 22:14:00 +0800

    refactor: make the ScriptManager more powerful

--- packages/base/src/ScriptManager.ts
+++ packages/base/src/ScriptManager.ts
@@ -1,6 +1,10 @@
 import { CellDep, Script, WitnessArgs } from '@ckb-lumos/base';
+import { ScriptConfig } from '@ckb-lumos/config-manager';
 
-export interface ConfigManager {
+export interface ScriptManager {
   getScriptsCellDeps(scripts: Script[]): CellDep[];
   getWitnessPlaceholder(script: Script): WitnessArgs;
+
+  getScriptConfig(key: string): ScriptConfig;
+  newScript(key: string): Script;
 }
