bf7d401e1c8e015a205024fe168db60ed49765e2
homura <homura.dev@gmail.com>
2021-08-09 20:17:22 +0800

    feat: supported mint with multi cells of the issuer

--- packages/ckit/src/tx-builders/pw/MintSudtPwBuilder.ts
+++ packages/ckit/src/tx-builders/pw/MintSudtPwBuilder.ts
@@ -2,29 +2,11 @@ import { Address } from '@ckb-lumos/base';
 import { Amount, Builder, Cell, RawTransaction, Transaction } from '@lay2/pw-core';
 import { from, lastValueFrom, mergeMap, toArray } from 'rxjs';
 import { MintOptions } from '..';
+import { NoAvailableCellError } from '../../errors';
 import { Pw } from '../../helpers/pw';
 import { CkitProvider } from '../../providers';
-import { asserts, boom } from '../../utils';
+import { byteLenOfCkbLiveCell, byteLenOfSudt } from '../builder-utils';
 import { AbstractPwSenderBuilder } from './AbstractPwSenderBuilder';
-
-// TODO dynamic lock args
-//  the capacity of lock args is assumed to be a fixed 20 bytes,
-//  it should be a calced value
-const SUDT_TYPE_SCRIPT_CAPACITY =
-  10n ** 8n *
-  BigInt(
-    8 /* capacity: u64 */ +
-      /* lock script */
-      32 /* code_hash: U256 */ +
-      20 /* lock_args: blake160 */ +
-      1 /* hash_type: u8 */ +
-      /* type script */
-      32 /* code_hash: U256 */ +
-      32 /* args: U256, issuer lock hash */ +
-      1 /* hash_type: u8 */ +
-      /* output_data */
-      16 /* data: u128, amount, little-endian */,
-  );
 
 export class NonAcpPwMintBuilder extends AbstractPwSenderBuilder {
   constructor(private options: MintOptions, provider: CkitProvider, private issuerAddress: Address) {
@@ -32,29 +14,15 @@ export class NonAcpPwMintBuilder extends AbstractPwSenderBuilder {
   }
 
   async build(): Promise<Transaction> {
-    const issuerAddress = this.issuerAddress;
+    const udtType = this.provider.newSudtScript(this.issuerAddress);
 
-    const cells = await this.provider.collectCkbLiveCells(issuerAddress, '1');
-    asserts(cells[0] != null);
-
-    const script = this.provider.parseToScript(issuerAddress);
-
-    const issuerLockScript = Pw.toPwScript(script);
-    const issuerCell = new Cell(
-      new Amount(cells[0].output.capacity, 0),
-      issuerLockScript,
-      undefined,
-      Pw.toPwOutPoint(cells[0].out_point),
-    );
-
-    const udtType = this.provider.newScript('SUDT', issuerLockScript.toHash());
-
+    // capacity provided by issuer
     const createdRecipientCells = this.options.recipients
       .filter((item) => item.capacityPolicy === 'createAcp')
       .map(
         (item) =>
           new Cell(
-            new Amount((BigInt(item.additionalCapacity || 0) + BigInt(SUDT_TYPE_SCRIPT_CAPACITY)).toString(), 0),
+            new Amount((BigInt(item.additionalCapacity || 0) + BigInt(byteLenOfSudt())).toString(), 0),
             Pw.toPwScript(this.provider.parseToScript(item.recipient)), // recipient lock
             Pw.toPwScript(udtType), // issued sudt type
             undefined,
@@ -62,12 +30,15 @@ export class NonAcpPwMintBuilder extends AbstractPwSenderBuilder {
           ),
       );
 
+    // capacity provided by recipient
     const resolvedFindAcpCells = this.options.recipients.filter((item) => item.capacityPolicy === 'findAcp');
     const foundRecipientCells$ = from(resolvedFindAcpCells).pipe(
       // collect the udt cell from recipients, throw an error if no udt cell is found
       mergeMap(async (item) => {
         const resolved = (await this.provider.collectUdtCells(item.recipient, udtType, '0'))[0];
-        if (!resolved) boom(`Cannot find available udt cells from ${item.recipient}`);
+        if (!resolved) {
+          throw new NoAvailableCellError({ lock: this.provider.parseToScript(item.recipient), type: udtType });
+        }
 
         const cell = Pw.toPwCell(resolved);
         cell.capacity = cell.capacity.add(new Amount(item.additionalCapacity || '0', 0));
@@ -79,16 +50,6 @@ export class NonAcpPwMintBuilder extends AbstractPwSenderBuilder {
     );
     const foundRecipientCells = await lastValueFrom(foundRecipientCells$);
 
-    const issuerChangeCell = issuerCell.clone();
-
-    const rawTx = new RawTransaction(
-      [issuerCell, ...foundRecipientCells],
-      [issuerChangeCell, ...createdRecipientCells, ...foundRecipientCells],
-      this.getCellDeps(),
-    );
-    const tx = new Transaction(rawTx, [this.getWitnessPlaceholder(issuerAddress)]);
-
-    const fee = Builder.calcFee(tx, Number(this.provider.config.MIN_FEE_RATE));
     const createdRecipientCellsCapacity = createdRecipientCells.reduce(
       (sum, createdCell) => sum.add(createdCell.capacity),
       Amount.ZERO,
@@ -98,7 +59,34 @@ export class NonAcpPwMintBuilder extends AbstractPwSenderBuilder {
       Amount.ZERO,
     );
 
-    issuerChangeCell.capacity = issuerChangeCell.capacity
+    const neededCapacityFromIssuer = createdRecipientCellsCapacity
+      .add(foundRecipientCellsAdditionalCapacity)
+      // additional 61 ckb to ensure capacity is enough for change cell
+      .add(new Amount(String(byteLenOfCkbLiveCell())))
+      // additional 1 ckb for tx fee, not all 1ckb will be paid,
+      // but the real fee will be calculated based on feeRate
+      .add(new Amount('1'));
+
+    const issuerOutpoints = await this.provider.collectCkbLiveCells(
+      this.issuerAddress,
+      neededCapacityFromIssuer.toHexString(),
+    );
+
+    const issuerCells = issuerOutpoints.map(Pw.toPwCell);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const issuerOutput = issuerCells[0]!.clone();
+    issuerOutput.capacity = issuerCells.reduce((sum, inputCell) => sum.add(inputCell.capacity), Amount.ZERO);
+
+    const rawTx = new RawTransaction(
+      [...issuerCells, ...foundRecipientCells],
+      [issuerOutput, ...createdRecipientCells, ...foundRecipientCells],
+      this.getCellDeps(),
+    );
+    const tx = new Transaction(rawTx, [this.getWitnessPlaceholder(this.issuerAddress)]);
+    const fee = Builder.calcFee(tx, Number(this.provider.config.MIN_FEE_RATE));
+
+    issuerOutput.capacity = issuerOutput.capacity
       .sub(fee)
       .sub(createdRecipientCellsCapacity)
       .sub(foundRecipientCellsAdditionalCapacity);
