ad8aa9a346474380ce53630d62ab03b1746a7a69
homura <homura.dev@gmail.com>
2021-08-12 14:04:43 +0800

    chore: production build presets

--- apps/ckit-app/craco.config.js
+++ apps/ckit-app/craco.config.js
@@ -1,7 +1,15 @@
 const CracoLessPlugin = require('craco-less');
 
+function getProductionSetting() {
+  if (process.env.NODE_ENV !== 'production') return {};
+  return {
+    presets: [['@babel/preset-env', { useBuiltIns: 'entry' }]],
+  };
+}
+
 module.exports = {
   babel: {
+    ...getProductionSetting(),
     plugins: ['babel-plugin-styled-components'],
   },
   devServer: {
